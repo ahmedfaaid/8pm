b6c228fc0605ce1a97eec72c8a5acbc36a19687d
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2023-02-02 19:46:04 -0500

    add container to nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,33 +4,35 @@ import logo from '../public/images/logo/black-logo.png';
 
 export default function Navbar() {
   return (
-    <nav className='absolute flex w-full items-center justify-between px-16 py-4'>
-      <div className='w-16'>
-        <Image src={logo} alt='8PM' priority />
-      </div>
-      <div>
-        <ul className='flex list-none text-lg uppercase text-smoky-black'>
-          <li>
-            <Link href='/work' className='p-2 hover:text-zinc-700'>
-              Work
-            </Link>
-          </li>
-          <li>
-            <Link href='/blog' className='p-2 hover:text-zinc-700'>
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Link href='/services' className='p-2 hover:text-zinc-700'>
-              Services
-            </Link>
-          </li>
-          <li>
-            <Link href='/contact' className='p-2 hover:text-zinc-700'>
-              Contact
-            </Link>
-          </li>
-        </ul>
+    <nav className='absolute w-full'>
+      <div className='container mx-auto flex items-center justify-between py-4'>
+        <div className='w-16'>
+          <Image src={logo} alt='8PM' priority />
+        </div>
+        <div>
+          <ul className='flex list-none text-lg uppercase text-smoky-black'>
+            <li>
+              <Link href='/work' className='p-2 hover:text-zinc-700'>
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link href='/blog' className='p-2 hover:text-zinc-700'>
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link href='/services' className='p-2 hover:text-zinc-700'>
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link href='/contact' className='p-2 hover:text-zinc-700'>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
