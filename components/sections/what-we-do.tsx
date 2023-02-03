0d34cf5ec7c70029e369f6fcc527a6aa07e9610d
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2023-02-03 16:09:50 -0500

    adjust grid gap

--- components/sections/what-we-do.tsx
+++ components/sections/what-we-do.tsx
@@ -5,30 +5,30 @@ export default function WhatWeDo() {
         <div>
           <p className='text-6xl font-medium'>What we do</p>
         </div>
-        <div className='col-span-2 grid grid-cols-2 grid-rows-2 gap-x-4 gap-y-10 text-xl font-medium'>
+        <div className='col-span-2 grid grid-cols-2 grid-rows-2 gap-x-6 gap-y-10 text-xl'>
           <div>
-            <p className='mb-6 text-3xl'>Brand Strategy</p>
+            <p className='mb-6 text-3xl font-medium'>Brand Strategy</p>
             <p>
               My posters capture attention with bold graphics, punchy text and
               on-brand visuals.
             </p>
           </div>
           <div>
-            <p className='mb-6 text-3xl'>Web Design</p>
+            <p className='mb-6 text-3xl font-medium'>Web Design</p>
             <p>
               Make them see! I create digital smorgasbords of color and images
               made larger than life.
             </p>
           </div>
           <div>
-            <p className='mb-6 text-3xl'>UX/UI Design</p>
+            <p className='mb-6 text-3xl font-medium'>UX/UI Design</p>
             <p>
               My mini murals turn blank spaces into visual messages that speak
               volumes.
             </p>
           </div>
           <div>
-            <p className='mb-6 text-3xl'>Custom</p>
+            <p className='mb-6 text-3xl font-medium'>Custom</p>
             <p>
               The medium is the message. Capture it all through inventive type.
             </p>
