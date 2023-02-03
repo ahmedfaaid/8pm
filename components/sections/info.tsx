4669908e3247bf2d659b2855c1e3cfe851394ed3
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2023-02-03 18:37:00 -0500

    responsive info section

--- components/sections/info.tsx
+++ components/sections/info.tsx
@@ -1,12 +1,16 @@
 export default function Info() {
   return (
     <section className='flex h-screen items-center justify-center bg-primary'>
-      <div className='container grid grid-cols-2 leading-tight tracking-wide text-white'>
-        <div className='font-medium'>
-          <p className='mb-20 text-4xl'>HI, WE ARE 8PM!</p>
-          <p className='text-6xl'>We design with purpose.</p>
+      <div className='container gap-x-1.5 px-4 leading-tight tracking-wide text-white sm:grid sm:grid-cols-2 sm:px-0 md:gap-x-0'>
+        <div className='mb-10 font-medium sm:mb-0'>
+          <p className='mb-10 text-center text-3xl sm:mb-20 sm:text-left md:text-4xl'>
+            HI, WE ARE 8PM!
+          </p>
+          <p className='text-center text-5xl sm:text-left md:text-6xl'>
+            We design with purpose.
+          </p>
         </div>
-        <div className='text-2xl'>
+        <div className='text-xl md:text-2xl'>
           <p className='mb-8'>
             The medium is the message, but the message also defines the medium.
             I am constantly experimenting with both, without limits or
