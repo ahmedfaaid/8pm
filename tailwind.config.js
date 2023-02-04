f72e3f33b7fb2dfa2de8c78afc9c91c1c8b77764
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2023-02-03 20:27:01 -0500

    primary shades

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,18 @@ module.exports = {
         sans: ['var(--font-inter)']
       },
       colors: {
-        primary: '#1B1F24',
+        primary: {
+          DEFAULT: '#1B1F24',
+          100: '#D1D2D3',
+          200: '#A4A5A7',
+          300: '#76797C',
+          400: '#494C50',
+          500: '#1B1F24',
+          600: '#16191D',
+          700: '#101316',
+          800: '#0B0C0E',
+          900: '#050607'
+        },
         'smoky-black': '#100C08'
       }
     }
