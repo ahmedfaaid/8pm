a294baf788c92fdc14f1a14d900ccd2885e8ba3f
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2023-02-03 22:19:45 -0500

    change siteurl

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,5 +1,5 @@
 export const defaultDescription =
   'We are 8PM. We are here to solve all your user experience needs.';
 export const defaultTitle = '8PM | Your user experience experts';
-export const siteUrl = 'https://weare8pm.com';
+export const siteUrl = 'https://8pm.vercel.app';
 export const twitterUsername = '@weare8pm';
